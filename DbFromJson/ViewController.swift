7ab21117e53a6df612d7baf3c3333ddbff135f18
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2019-07-22 12:54:57 -0300

    local commit

--- DbFromJson/ViewController.swift
+++ DbFromJson/ViewController.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    // MARK: - UI Actions
+    @IBAction func generateDB(_ sender: Any) {
+        let cities = readJSONFromFile(fileName: "cidades")
+        print("🎁 cities: \(cities)")
+    }
+    
+    
+    // MARK: - Local JSON Reader
+    private func readJSONFromFile(fileName: String) -> [City] {
+        var cities = [City]()
 
+        if let path = Bundle.main.path(forResource: fileName, ofType: "json") {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+                let json = try! JSONSerialization.jsonObject(with: data, options: [])
+                let dataArray : NSArray = json as! NSArray
+                
+                for item in dataArray {
+                    let dic = item as! NSDictionary
+                    let city = City(dictionary: dic)
+                    cities.append(city!)
+                }
+
+            } catch {
+                // handle error
+                print("error: \(error)")
+                print("....")
+            }
+        }
+        
+        
+        return cities
+    }
+    
 }
 
